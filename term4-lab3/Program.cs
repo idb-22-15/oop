f99403ccdb1c0fd6f14102338fec42327d234789
Artemiy111 <mrart111@mail.ru>
2024-05-03 15:30:49 +0300

    lint

--- term4-lab3/Program.cs
+++ term4-lab3/Program.cs
@@ -7,90 +7,104 @@
 
 class Program
 {
-    static void Main()
-    {
-      task1();
-      task2();
-      task3();
-      task4();
-      task5();
-    }
+  static void Main()
+  {
+    task1();
+    task2();
+    task3();
+    task4();
+    task5();
+  }
 
-    static void task1() {
-      
-      Person person = new Person();
+  static void task1()
+  {
 
-      // Получение ссылки на объект
-      Person reference = person;
+    Person person = new Person();
 
-      // Использование ссылки для доступа к полям и методам объекта
-      
-      Console.WriteLine(reference.Name);
-      reference.Name = "qw";
-      Console.WriteLine(person.Name);
+    // Получение ссылки на объект
+    Person reference = person;
 
-    }
+    // Использование ссылки для доступа к полям и методам объекта
 
-    static void task2() {
-      MyClass m = new MyClass();
-      MyClass o = new OtherClass();
-      m.doSomething();
-      o.doSomething();
-    }
+    Console.WriteLine(reference.Name);
+    reference.Name = "qw";
+    Console.WriteLine(person.Name);
 
-    static void task3() {
-      var p = new MyPartial();
-      p.hello();
-      p.world();
-    }
+  }
 
-    static void task4() {
-      var s = new MySealed();
-      s.sealedHello();
-    }
+  static void task2()
+  {
+    MyClass m = new MyClass();
+    MyClass o = new OtherClass();
+    m.doSomething();
+    o.doSomething();
+  }
 
-    static void task5() {
-        Animal dog = new Dog();
-        Animal cat = new Cat();
+  static void task3()
+  {
+    var p = new MyPartial();
+    p.hello();
+    p.world();
+  }
 
-        dog.MakeSound();
-        cat.MakeSound();
-        dog.Sleep();
-        cat.Sleep();
-    }
+  static void task4()
+  {
+    var s = new MySealed();
+    s.sealedHello();
+  }
+
+  static void task5()
+  {
+    Animal dog = new Dog();
+    Animal cat = new Cat();
+
+    dog.MakeSound();
+    cat.MakeSound();
+    dog.Sleep();
+    cat.Sleep();
+  }
 }
 
 // 1
-class Person {
+class Person
+{
   public string Name = "ppp";
 }
 
 
 // 2
 
-class MyClass {
-  public virtual void doSomething() {
+class MyClass
+{
+  public virtual void doSomething()
+  {
     Console.WriteLine("virtual Hello");
   }
 }
 
-class OtherClass : MyClass {
-  public override void doSomething() {
+class OtherClass : MyClass
+{
+  public override void doSomething()
+  {
     Console.WriteLine("override World");
   }
 }
 
 // 3
 
-partial class MyPartial {
-  public void hello() {
+partial class MyPartial
+{
+  public void hello()
+  {
     Console.WriteLine("partial Hello");
 
   }
 }
 
-partial class MyPartial {
-  public void world() {
+partial class MyPartial
+{
+  public void world()
+  {
     Console.WriteLine("partial World");
   }
 }
@@ -98,20 +112,26 @@ partial class MyPartial {
 
 // 4
 
-sealed class MySealed {
-  public void sealedHello() {
+sealed class MySealed
+{
+  public void sealedHello()
+  {
     Console.WriteLine("sealed Hello");
   }
 }
 
-class NotSealed {
-  public virtual void doSomething() {
+class NotSealed
+{
+  public virtual void doSomething()
+  {
     Console.WriteLine("not seeled virtual Hello");
   }
 }
 
-class WithSealedMethod : NotSealed {
-  public sealed override void doSomething() {
+class WithSealedMethod : NotSealed
+{
+  public sealed override void doSomething()
+  {
     Console.WriteLine("sealed override World");
   }
 }
@@ -127,31 +147,31 @@ class WithSealedMethod : NotSealed {
 
 public abstract class Animal
 {
-    // Абстрактный метод, представляющий голос животного
-    public abstract void MakeSound();
+  // Абстрактный метод, представляющий голос животного
+  public abstract void MakeSound();
 
-    // Обычный метод
-    public void Sleep()
-    {
-        Console.WriteLine("Zzz");
-    }
+  // Обычный метод
+  public void Sleep()
+  {
+    Console.WriteLine("Zzz");
+  }
 }
 
 public class Dog : Animal
 {
-    // Реализация абстрактного метода MakeSound для собаки
-    public override void MakeSound()
-    {
-        Console.WriteLine("Woof");
-    }
+  // Реализация абстрактного метода MakeSound для собаки
+  public override void MakeSound()
+  {
+    Console.WriteLine("Woof");
+  }
 }
 
 public class Cat : Animal
 {
-    // Реализация абстрактного метода MakeSound для кошки
-    public override void MakeSound()
-    {
-        Console.WriteLine("Meow");
-    }
+  // Реализация абстрактного метода MakeSound для кошки
+  public override void MakeSound()
+  {
+    Console.WriteLine("Meow");
+  }
 }
 
--- term4-lab4/Program.cs
+++ term4-lab4/Program.cs
@@ -4,94 +4,99 @@ using System.Reflection;
 
 class Program
 {
-    static void Main()
+  static void Main()
+  {
+    task0();
+    task1();
+    task2();
+    task4();
+    task5();
+  }
+  static void task0()
+  {
+    int[] array = { 1, 4, 6, 7, 9 };
+
+    // Создание экземпляра коллекции
+    MyCollection collection = new MyCollection(array);
+
+    // Перебор элементов коллекции с помощью IEnumerator
+    IEnumerator enumerator = collection.GetEnumerator();
+    while (enumerator.MoveNext())
     {
-       task0();
-       task1();
-       task2();
-       task4();
-       task5();
+      Console.WriteLine(enumerator.Current);
     }
-    static void task0() {
-        int[] array = { 1, 4, 6, 7, 9 };
+  }
 
-        // Создание экземпляра коллекции
-        MyCollection collection = new MyCollection(array);
-        
-        // Перебор элементов коллекции с помощью IEnumerator
-        IEnumerator enumerator = collection.GetEnumerator();
-        while (enumerator.MoveNext())
-        {
-            Console.WriteLine(enumerator.Current);
-        }
-    }
+  static void task1()
+  {
+    // Получаем тип класса ReflectionExample
+    Type type = typeof(ReflectionExample);
 
-    static void task1() {
-        // Получаем тип класса ReflectionExample
-        Type type = typeof(ReflectionExample);
+    // Выводим имя класса
+    Console.WriteLine("Имя класса: " + type.Name);
 
-        // Выводим имя класса
-        Console.WriteLine("Имя класса: " + type.Name);
-
-        // Получаем все публичные методы класса
-        MethodInfo[] methods = type.GetMethods();
-        Console.WriteLine("Публичные методы:");
-        foreach (var method in methods)
-        {
-            Console.WriteLine(method.Name);
-        }
-
-        // Получаем все публичные свойства класса
-        PropertyInfo[] properties = type.GetProperties();
-        Console.WriteLine("Публичные свойства:");
-        foreach (var property in properties)
-        {
-            Console.WriteLine(property.Name);
-        }
+    // Получаем все публичные методы класса
+    MethodInfo[] methods = type.GetMethods();
+    Console.WriteLine("Публичные методы:");
+    foreach (var method in methods)
+    {
+      Console.WriteLine(method.Name);
     }
 
-    static void task2() {
-         // Создание объектов через интерфейсные ссылки
-        IShape shape1 = new Circle(5);
-        IShape shape2 = new Rectangle(4, 6);
+    // Получаем все публичные свойства класса
+    PropertyInfo[] properties = type.GetProperties();
+    Console.WriteLine("Публичные свойства:");
+    foreach (var property in properties)
+    {
+      Console.WriteLine(property.Name);
+    }
+  }
 
-        // Вычисление площади для каждой фигуры
-        Console.WriteLine("Площадь круга: " + shape1.Area());
-        Console.WriteLine("Площадь прямоугольника: " + shape2.Area());
+  static void task2()
+  {
+    // Создание объектов через интерфейсные ссылки
+    IShape shape1 = new Circle(5);
+    IShape shape2 = new Rectangle(4, 6);
+
+    // Вычисление площади для каждой фигуры
+    Console.WriteLine("Площадь круга: " + shape1.Area());
+    Console.WriteLine("Площадь прямоугольника: " + shape2.Area());
+  }
+
+  static void task4()
+  {
+    // Используем итераторный метод для получения четных чисел
+    Console.WriteLine("Четные числа до 10:");
+    foreach (var number in NumberGenerator.GetEvenNumbers(10)) // четные числа от 0 до 10
+    {
+      Console.WriteLine(number);
     }
 
-    static void task4() {
-         // Используем итераторный метод для получения четных чисел
-        Console.WriteLine("Четные числа до 10:");
-        foreach (var number in NumberGenerator.GetEvenNumbers(10)) // четные числа от 0 до 10
-        {
-            Console.WriteLine(number);
-        }
-
-        // Используем итераторный метод для получения чисел Фибоначчи
-        Console.WriteLine("\nЧисла Фибоначчи (первые 10):");
-        foreach (var number in NumberGenerator.GetFibonacci(10)) // первые 10 чисел Фибоначчи
-        {
-            Console.WriteLine(number);
-        }
+    // Используем итераторный метод для получения чисел Фибоначчи
+    Console.WriteLine("\nЧисла Фибоначчи (первые 10):");
+    foreach (var number in NumberGenerator.GetFibonacci(10)) // первые 10 чисел Фибоначчи
+    {
+      Console.WriteLine(number);
     }
+  }
 
-    static void task5() {
-        Bookstore myBookstore = new Bookstore();
+  static void task5()
+  {
+    Bookstore myBookstore = new Bookstore();
 
-        // Установка значений книг с использованием индексатора
-        myBookstore[0] = "Book 1";
-        myBookstore[1] = "Book 2";
-        myBookstore[2] = "Book 3";
+    // Установка значений книг с использованием индексатора
+    myBookstore[0] = "Book 1";
+    myBookstore[1] = "Book 2";
+    myBookstore[2] = "Book 3";
 
-        // Выведет "Invalid index"
-        myBookstore[15] = "Book 15";
+    // Выведет "Invalid index"
+    myBookstore[15] = "Book 15";
 
-        // Получение значений книг с использованием индексатора
-        Console.WriteLine("Book at index 0: " + myBookstore[0]);
-        Console.WriteLine("Book at index 1: " + myBookstore[1]);
-        Console.WriteLine("Book at index 2: " + myBookstore[2]);
-    }
+    // Получение значений книг с использованием индексатора
+    Console.WriteLine("Book at index 0: " + myBookstore[0]);
+    Console.WriteLine("Book at index 1: " + myBookstore[1]);
+    Console.WriteLine("Book at index 2: " + myBookstore[2]);
+  }
 }
 
 
@@ -99,53 +104,53 @@ class Program
 
 public class MyCollection : IEnumerable
 {
-    private int[] data;
+  private int[] data;
 
-    public MyCollection(int[] data)
-    {
-        this.data = data;
-    }
+  public MyCollection(int[] data)
+  {
+    this.data = data;
+  }
 
-    public IEnumerator GetEnumerator()
-    {
-        return new MyEnumerator(data);
-    }
+  public IEnumerator GetEnumerator()
+  {
+    return new MyEnumerator(data);
+  }
 }
 
 
 // Класс, реализующий интерфейс IEnumerator
 public class MyEnumerator : IEnumerator
 {
-    private int[] data;
-    private int position = -1;
+  private int[] data;
+  private int position = -1;
 
-    // Конструктор, принимающий массив данных
-    public MyEnumerator(int[] data)
-    {
-        this.data = data;
-    }
+  // Конструктор, принимающий массив данных
+  public MyEnumerator(int[] data)
+  {
+    this.data = data;
+  }
 
-    // Реализация свойства Current интерфейса IEnumerator
-    public object Current
+  // Реализация свойства Current интерфейса IEnumerator
+  public object Current
+  {
+    get
     {
-        get
-        {
-            if (position == -1 || position >= data.Length)
-                throw new InvalidOperationException();
-            return data[position];
-        }
+      if (position == -1 || position >= data.Length)
+        throw new InvalidOperationException();
+      return data[position];
     }
+  }
 
-    public bool MoveNext()
-    {
-        position++;
-        return (position < data.Length);
-    }
+  public bool MoveNext()
+  {
+    position++;
+    return (position < data.Length);
+  }
 
-    public void Reset()
-    {
-        position = -1;
-    }
+  public void Reset()
+  {
+    position = -1;
+  }
 }
 
 
@@ -153,10 +158,10 @@ public class MyEnumerator : IEnumerator
 
 class ReflectionExample
 {
-    public void Method1() { }
-    public void Method2() { }
-    public int Property1 { get; set; }
-    public string Property2 { get; set; }
+  public void Method1() { }
+  public void Method2() { }
+  public int Property1 { get; set; }
+  public string Property2 { get; set; }
 }
 
 // 2
@@ -165,43 +170,43 @@ class ReflectionExample
 // Определение интерфейса
 public interface IShape
 {
-    double Area(); // Метод для вычисления площади фигуры
+  double Area(); // Метод для вычисления площади фигуры
 }
 
 // Реализация интерфейса в классе Circle
 public class Circle : IShape
 {
-    private double radius;
+  private double radius;
 
-    public Circle(double radius)
-    {
-        this.radius = radius;
-    }
+  public Circle(double radius)
+  {
+    this.radius = radius;
+  }
 
-    // Реализация метода Area() для круга
-    public double Area()
-    {
-        return Math.PI * radius * radius;
-    }
+  // Реализация метода Area() для круга
+  public double Area()
+  {
+    return Math.PI * radius * radius;
+  }
 }
 
 // Реализация интерфейса в классе Rectangle
 public class Rectangle : IShape
 {
-    private double length;
-    private double width;
+  private double length;
+  private double width;
 
-    public Rectangle(double length, double width)
-    {
-        this.length = length;
-        this.width = width;
-    }
+  public Rectangle(double length, double width)
+  {
+    this.length = length;
+    this.width = width;
+  }
 
-    // Реализация метода Area() для прямоугольника
-    public double Area()
-    {
-        return length * width;
-    }
+  // Реализация метода Area() для прямоугольника
+  public double Area()
+  {
+    return length * width;
+  }
 }
 
 // 3
@@ -212,29 +217,29 @@ public class Rectangle : IShape
 
 public class NumberGenerator
 {
-    // Итераторный метод, который возвращает четные числа от 0 до указанного предела
-    public static IEnumerable<int> GetEvenNumbers(int max)
+  // Итераторный метод, который возвращает четные числа от 0 до указанного предела
+  public static IEnumerable<int> GetEvenNumbers(int max)
+  {
+    for (int i = 0; i <= max; i += 2) // перебираем все четные числа
     {
-        for (int i = 0; i <= max; i += 2) // перебираем все четные числа
-        {
-            yield return i; // возвращаем число, используем ключевое слово yield
-        }
+      yield return i; // возвращаем число, используем ключевое слово yield
     }
+  }
 
-    // Итераторный метод, который возвращает числа Фибоначчи до определенного количества
-    public static IEnumerable<int> GetFibonacci(int count)
+  // Итераторный метод, который возвращает числа Фибоначчи до определенного количества
+  public static IEnumerable<int> GetFibonacci(int count)
+  {
+    int a = 0;
+    int b = 1;
+
+    for (int i = 0; i < count; i++) // создаем последовательность чисел Фибоначчи
     {
-        int a = 0;
-        int b = 1;
-
-        for (int i = 0; i < count; i++) // создаем последовательность чисел Фибоначчи
-        {
-            yield return a; // возвращаем текущее число Фибоначчи
-            int temp = a + b;
-            a = b;
-            b = temp;
-        }
+      yield return a; // возвращаем текущее число Фибоначчи
+      int temp = a + b;
+      a = b;
+      b = temp;
     }
+  }
 }
 
 // 5
@@ -243,40 +248,40 @@ public class NumberGenerator
 
 public class Bookstore
 {
-    private string[] books;
+  private string[] books;
 
-    // Конструктор
-    public Bookstore()
-    {
-        books = new string[10]; // Инициализируем массив для 10 книг
-    }
+  // Конструктор
+  public Bookstore()
+  {
+    books = new string[10]; // Инициализируем массив для 10 книг
+  }
 
-    // Индексатор для доступа к книгам по индексу
-    public string this[int index]
+  // Индексатор для доступа к книгам по индексу
+  public string this[int index]
+  {
+    get
     {
-        get
-        {
-            // Проверяем допустимость индекса
-            if (index >= 0 && index < books.Length)
-            {
-                return books[index];
-            }
-            else
-            {
-                throw new Exception("lol no");
-            }
-        }
-        set
-        {
-            // Проверяем допустимость индекса
-            if (index >= 0 && index < books.Length)
-            {
-                books[index] = value;
-            }
-            else
-            {
-                Console.WriteLine("Invalid index");
-            }
-        }
+      // Проверяем допустимость индекса
+      if (index >= 0 && index < books.Length)
+      {
+        return books[index];
+      }
+      else
+      {
+        throw new Exception("lol no");
+      }
     }
+    set
+    {
+      // Проверяем допустимость индекса
+      if (index >= 0 && index < books.Length)
+      {
+        books[index] = value;
+      }
+      else
+      {
+        Console.WriteLine("Invalid index");
+      }
+    }
+  }
 }
