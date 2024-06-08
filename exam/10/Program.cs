5d80ffcdce6a15fd1c43ef7f58216a041c1d51dd
Artemiy111 <mrart111@mail.ru>
2024-06-08 21:46:43 +0300

    10

---
+++ exam/10/Program.cs
@@ -0,0 +1,34 @@
+﻿
+interface IO {
+  string Read();
+  void Write(string str);
+  void Log();
+}
+
+class MyIO: IO {
+  public string Read() {
+    return "world";
+  }
+  public void Write(string str) {
+    Console.WriteLine("hello " + str);
+  }
+  public void Log() {
+    Console.WriteLine("log");
+  }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создание экземпляра класса MyClass
+        MyIO myIo = new MyIO();
+
+        // Использование интерфейсной ссылки для вызова методов
+        IO io = myIo;
+        string s = io.Read();
+        io.Write(s);
+        io.Log();
+
+    }
+}
