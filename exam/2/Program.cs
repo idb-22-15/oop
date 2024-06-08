18a408b5c48de8d773adc81f91a06cc6d81427ff
Artemiy111 <mrart111@mail.ru>
2024-06-08 21:07:28 +0300

    2

---
+++ exam/2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class MyClass
+{
+    // Приватный метод для вывода сообщения на консоль
+    private void PrintMessage(string message)
+    {
+        Console.WriteLine("Сообщение из приватного метода: " + message);
+    }
+
+    // Публичный метод, который вызывает приватный метод
+    public void CallPrivateMethod(string message)
+    {
+        PrintMessage(message); // Вызов приватного метода из публичного метода
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass();
+        myObject.CallPrivateMethod("Привет, мир!"); // Вызов публичного метода, который в свою очередь вызывает приватный метод
+    }
+}
