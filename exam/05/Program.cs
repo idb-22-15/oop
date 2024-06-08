a4717ba78cbb4c2977758db60e199beae6856e53
Artemiy111 <mrart111@mail.ru>
2024-06-08 21:24:15 +0300

    5

---
+++ exam/05/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// Базовый класс с виртуальным и абстрактным методами
+abstract class BaseClass
+{
+    // Абстрактный метод
+    public abstract void AbstractMethod();
+
+    // Виртуальный метод
+    public virtual void VirtualMethod()
+    {
+        Console.WriteLine("Вызван виртуальный метод из базового класса");
+    }
+}
+
+// Наследник от базового класса
+class DerivedClass : BaseClass
+{
+    // Переопределение абстрактного метода
+    public override void AbstractMethod()
+    {
+        Console.WriteLine("Вызван переопределенный абстрактный метод из класса-наследника");
+    }
+
+    // Переопределение виртуального метода
+    public override void VirtualMethod()
+    {
+        Console.WriteLine("Вызван переопределенный виртуальный метод из класса-наследника");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создание объекта класса-наследника
+        DerivedClass derived = new DerivedClass();
+
+        // Вызов переопределенных методов
+        derived.AbstractMethod();
+        derived.VirtualMethod();
+    }
+}
