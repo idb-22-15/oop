75cb33fc85c5ea444a1e96eeca523c79203030ba
Artemiy111 <mrart111@mail.ru>
2024-06-22 05:31:27 +0300

    .

--- exam/15/Program.cs
+++ exam/15/Program.cs
@@ -1,73 +1,68 @@
 ﻿using System;
 using System.Threading;
-using System.Threading.Tasks;
-
+using System.Diagnostics;
 class Program
 {
   static void Main(string[] args)
   {
     int numThreads = 10;
     int[] executionTimes = new int[numThreads];
-
-    Task[] tasks = new Task[numThreads];
+    Thread[] threads = new Thread[numThreads];
 
     for (int i = 0; i < numThreads; i++)
     {
       int threadIndex = i;
-      tasks[i] = Task.Run(() =>
+      threads[i] = new Thread(() =>
       {
         int targetValue = new Random().Next(0, 1001);
         int arraySize = new Random().Next(10000000, 15000001);
-        int[] array = GenerateRandomArray(arraySize, 0, 1000);
+        int[] array = GenerateRandomArray(arraySize, 0, 1001);
 
-        var watch = System.Diagnostics.Stopwatch.StartNew();
+        var watch = Stopwatch.StartNew();
         int count = CountElementsEqualToX(array, targetValue);
         watch.Stop();
 
         executionTimes[threadIndex] = (int)watch.ElapsedMilliseconds;
         Console.WriteLine($"Поток {threadIndex + 1}: Количество элементов, равных {targetValue}: {count}. Время выполнения: {executionTimes[threadIndex]} мс");
       });
-    }
+      threads[i].Start();
 
-    // Ждем завершения всех задач
-    Task.WaitAll(tasks);
-
-    // Вычисляем минимальное, максимальное и среднее время выполнения
-    int minTime = executionTimes.Min();
-    int maxTime = executionTimes.Max();
-    int avgTime = (int)executionTimes.Average();
-
-    Console.WriteLine($"Минимальное время выполнения: {minTime} мс");
-    Console.WriteLine($"Максимальное время выполнения: {maxTime} мс");
-    Console.WriteLine($"Среднее время выполнения: {avgTime} мс");
-  }
-
-  static int[] GenerateRandomArray(int size, int min, int max)
-  {
-    int[] array = new int[size];
-    var random = new Random();
-    for (int i = 0; i < size; i++)
-    {
-      array[i] = random.Next(min, max + 1);
-    }
-    return array;
-  }
-
-  static int CountElementsEqualToX(int[] array, int x)
-  {
-    Array.Sort(array);
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-      if (array[i] == x)
+      foreach (Thread thread in threads)
       {
-        count++;
+        thread.Join();
       }
-      else if (array[i] > x)
-      {
-        break;
-      }
+
+      int minTime = executionTimes.Min();
+      int maxTime = executionTimes.Max();
+      int avgTime = (int)executionTimes.Average();
+
+      Console.WriteLine($"Минимальное время выполнения: {minTime} мс");
+      Console.WriteLine($"Максимальное время выполнения: {maxTime} мс");
+      Console.WriteLine($"Среднее время выполнения: {avgTime} мс");
     }
-    return count;
+
+    static int[] GenerateRandomArray(int size, int min, int max)
+    {
+      int[] array = new int[size];
+      var random = new Random();
+      for (int i = 0; i < size; i++)
+      {
+        array[i] = random.Next(min, max + 1);
+      }
+      return array;
+    }
+
+    static int CountElementsEqualToX(int[] array, int x)
+    {
+      int count = 0;
+      for (int i = 0; i < array.Length; i++)
+      {
+        if (array[i] == x)
+        {
+          count++;
+        }
+      }
+      return count;
+    }
   }
 }
