624213be826485ebabaf70798e013c93397c3c8a
Artemiy111 <mrart111@mail.ru>
2024-06-22 06:01:45 +0300

    .

---
+++ exam/17/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var t = new Thread(() => WaitForUserInput());
+
+    Console.WriteLine("Нажмите Enter");
+    t.Start();
+    t.Join();
+
+  }
+
+  static void WaitForUserInput()
+  {
+    while (true)
+    {
+      if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter)
+      {
+        Console.WriteLine("Вы нажали Enter.");
+      }
+    }
+  }
+}
